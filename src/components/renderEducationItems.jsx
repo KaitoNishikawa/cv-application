942e946ce8366a97acc537e5b513ce2ed39b21eb
kaitonishikawa <kai.nishikawa808@gmail.com>
2024-09-07 22:41:51 -0500

    made it responsive

--- src/components/renderEducationItems.jsx
+++ src/components/renderEducationItems.jsx
@@ -1,8 +1,14 @@
 export default function EducationItem({details, handleChange, handleMouseOverWrapper, handleMouseOverX, handleXButton}){
     const list = details
+    let isPhone = window.innerWidth < 500
     const listItems = list.map((item) => 
         <div key={item.id} className="items-wrapper" onMouseEnter={(e) => handleMouseOverWrapper(e, 'enter')} onMouseOut={(e) => handleMouseOverWrapper(e, 'out')}>
-            <button className="items-x" style={{visibility: 'hidden'}} onMouseEnter={(e) => handleMouseOverX(e, 'enter')} onMouseOut={(e) => handleMouseOverX(e, 'out')} onClick={(e) => handleXButton(e, 'ed')}>x</button>
+            {isPhone ? (
+                <button className="items-x" style={{visibility: 'visible'}} onMouseEnter={(e) => handleMouseOverX(e, 'enter')} onMouseOut={(e) => handleMouseOverX(e, 'out')} onClick={(e) => handleXButton(e, 'ed')}>x</button>
+            ) : (
+                <button className="items-x" style={{visibility: 'hidden'}} onMouseEnter={(e) => handleMouseOverX(e, 'enter')} onMouseOut={(e) => handleMouseOverX(e, 'out')} onClick={(e) => handleXButton(e, 'ed')}>x</button>
+            )}
+            
             <div className="items" key={item.id} onClick={handleChange()}>                
                 <div className="item-names" id={item.id}>{item.school}</div>
             </div>
--- src/components/renderExperienceItems.jsx
+++ src/components/renderExperienceItems.jsx
@@ -1,8 +1,14 @@
 export default function ExperienceItem({details, handleChange, handleMouseOverWrapper, handleMouseOverX, handleXButton}){
     const list = details
+    let isPhone = window.innerWidth < 500
     const listItems = list.map((item) => 
         <div key={item.id} className="items-wrapper" onMouseEnter={(e) => handleMouseOverWrapper(e, 'enter')} onMouseOut={(e) => handleMouseOverWrapper(e, 'out')}>
-            <button className="items-x" style={{visibility: 'hidden'}} onMouseEnter={(e) => handleMouseOverX(e, 'enter')} onMouseOut={(e) => handleMouseOverX(e, 'out')} onClick={(e) => handleXButton(e, 'ex')}>x</button>
+            {isPhone ? (
+                <button className="items-x" style={{visibility: 'visible'}} onMouseEnter={(e) => handleMouseOverX(e, 'enter')} onMouseOut={(e) => handleMouseOverX(e, 'out')} onClick={(e) => handleXButton(e, 'ex')}>x</button>
+            ) : (
+                <button className="items-x" style={{visibility: 'hidden'}} onMouseEnter={(e) => handleMouseOverX(e, 'enter')} onMouseOut={(e) => handleMouseOverX(e, 'out')} onClick={(e) => handleXButton(e, 'ex')}>x</button>
+            )}
+            
             <div className="items" key={item.id} onClick={handleChange()}>                
                 <div className="item-names" id={item.id}>{item.companyName}</div>
             </div>
